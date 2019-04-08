ca00c232658e6159cc9c934ca277aca22ba4a672
shektor <44544977+shektor@users.noreply.github.com>
2019-04-08 16:32:00 +0100

    Test max value error and pass

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,12 +1,15 @@
 class Oyster
 
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    raise("Balance will exceed £#{MAX_BALANCE} maximum") if (@balance + money) > MAX_BALANCE
+
     @balance += money
   end
 
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,14 +1,19 @@
 require 'oyster'
 
 describe Oyster do
-  oyster = Oyster.new
-
   context '#initialize' do
     it { expect(subject).to have_attributes(:balance => 0) }
   end
 
   context '#top up' do
+    oyster = Oyster.new
     it { expect(oyster.top_up(5)).to eq 5 }
     it { expect(oyster.top_up(10)).to eq 15 }
+  end
+  context 'max balance exceeded' do
+    it 'raises error' do
+      subject.top_up(90)
+      expect { expect(subject.top_up(1)) }.to raise_error("Balance will exceed £#{Oyster::MAX_BALANCE} maximum")
+    end
   end
 end
