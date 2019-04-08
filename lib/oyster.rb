6742736406c0d18ba816f26f4c607b7c95a7af5e
shektor <44544977+shektor@users.noreply.github.com>
2019-04-08 15:25:20 +0100

    Test initial balance and pass

---
+++ lib/oyster.rb
@@ -0,0 +1,8 @@
+class Oyster
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,7 @@
-describe Oyster do
+require 'oyster'
 
+describe Oyster do
+  context 'initialize' do
+    it { expect(subject).to have_attributes(:balance => 0) }
+  end
 end
